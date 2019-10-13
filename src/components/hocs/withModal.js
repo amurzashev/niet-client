3f26e4814e51d412905906dd7a9b8496d97891b9
Abylay Murzashev <abylay.murzashev@gmail.com>
2019-10-13 20:53:31 +0600

    full modal setup

--- src/components/hocs/withModal.js
+++ src/components/hocs/withModal.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Modal from '../molecules/Modal';
 
-export default (Component) => props => {
-  return (
-    <>
-      <Modal />
-      <Component {...props} />
-    </>
-  );
+export default ConnectedComponent => {
+  const Component = props => {
+    const { modal } = props;
+    return (
+      <>
+        { modal.isOpen ? <Modal /> : null }
+        <ConnectedComponent {...props} />
+      </>
+    );
+  };
+  Component.propTypes = {
+    modal: PropTypes.shape({
+      isOpen: PropTypes.bool,
+    }).isRequired,
+  };
+  const mapStateToProps = state => state;
+  return connect(mapStateToProps)(Component);
 };
--- src/duck/reducers/index.js
+++ src/duck/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import client from './client';
+import modal from './modal';
 
 export default combineReducers({
   client,
+  modal,
 });
