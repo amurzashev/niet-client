c19de904c193a1855d5714ae1cc410523649ad1b
Abylay Murzashev <abylay.murzashev@gmail.com>
2019-10-19 18:02:36 +0600

    additional entities redux store setup

---
+++ src/duck/reducers/additional.js
@@ -0,0 +1,15 @@
+const intialState = {
+  clients: [
+    // { iin: '', privilege: false }
+  ],
+  cars: [
+    // 'license plate num'
+  ],
+};
+
+export default (state = intialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
--- src/duck/reducers/index.js
+++ src/duck/reducers/index.js
@@ -3,10 +3,12 @@ import client from './client';
 import modal from './modal';
 import phone from './phone';
 import car from './car';
+import additional from './additional';
 
 export default combineReducers({
   client,
   modal,
   phone,
   car,
+  additional,
 });
