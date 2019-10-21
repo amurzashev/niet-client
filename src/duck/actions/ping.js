965c85f5d134b5e8ff6e0a74a9e434c388d438b9
Abylay Murzashev <abylay.murzashev@gmail.com>
2019-10-21 12:39:20 +0600

    clean up

--- src/duck/actions/ping.js
+++ src/duck/actions/ping.js
@@ -2,7 +2,5 @@ import { apiRequest } from '.';
 
 
 export default () => {
-  apiRequest.post('ping')
-    .then(res => console.log(res))
-    .catch(er => console.log(er.response));
+  apiRequest.post('ping');
 };
