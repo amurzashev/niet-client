0a769b7ad4a7889d553226360c34d126cdb5e885
Abylay Murzashev <abylay.murzashev@gmail.com>
2019-10-22 15:58:45 +0600

    heroku temp server link

--- src/duck/actions/index.js
+++ src/duck/actions/index.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV === 'development') {
   baseURL = 'http://localhost:3001/api/v1/';
 }
 if (process.env.NODE_ENV === 'production') {
-  baseURL = 'https://api.niet.kz/v1/';
+  baseURL = 'https://niet-server.herokuapp.com/api/v1/';
 }
 
 export const apiRequest = axios.create({
