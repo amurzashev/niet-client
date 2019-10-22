bd590cf2e3e66556d4dda7e6de4bccb1e08f8924
Abylay Murzashev <abylay.murzashev@gmail.com>
2019-10-22 17:15:50 +0600

    ticks

--- src/components/pages/Home/components/Information/components/Inputs/components/Ticks/index.js
+++ src/components/pages/Home/components/Information/components/Inputs/components/Ticks/index.js
@@ -5,18 +5,16 @@ import { connect } from 'react-redux';
 import Caption from 'components/atoms/Caption';
 import { setRulesTick, setDataTick } from 'duck/actions/client';
 
-const Ticks = ({ client, bindDataTick, bindRulesTick }) => {
-  return (
-    <>
-      <Input type="checkbox" tick={client.rulesTick} toggleTick={bindRulesTick}>
-        <Caption size="m">Ознакомлен с анкетой</Caption>
-      </Input>
-      <Input type="checkbox" tick={client.dataTick} toggleTick={bindDataTick}>
-        <Caption size="m">Я соглашаюсь на сбор и обработку моих данных</Caption>
-      </Input>
-    </>
-  );
-};
+const Ticks = ({ client, bindDataTick, bindRulesTick }) => (
+  <>
+    <Input type="checkbox" tick={client.rulesTick} toggleTick={bindRulesTick}>
+      <Caption size="m">Ознакомлен с анкетой</Caption>
+    </Input>
+    <Input type="checkbox" tick={client.dataTick} toggleTick={bindDataTick}>
+      <Caption size="m">Я соглашаюсь на сбор и обработку моих данных</Caption>
+    </Input>
+  </>
+);
 
 const mapStateToProps = state => ({ client: state.client });
 const mapDispatchToProps = {
