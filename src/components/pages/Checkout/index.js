59ecf9df2e6cfe1062ad58b19d2f8b5e3d90ca45
Abylay Murzashev <abylay.murzashev@gmail.com>
2019-10-22 17:59:48 +0600

    some checks before render

--- src/components/pages/Checkout/index.js
+++ src/components/pages/Checkout/index.js
@@ -1,19 +1,26 @@
 /* eslint-disable react/prop-types */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import Standard from 'components/templates/Standard';
 import Form from 'components/molecules/Form';
 
 const Checkout = props => {
+  const [visible, setVisible] = useState(false);
   useEffect(() => {
     const { client, car, history } = props;
     if (!client.name || !car.vin) {
       history.push('/');
+    } else {
+      setVisible(true);
     }
   }, []);
   const onSubmit = e => {
     e.preventDefault();
   };
+
+  if (!visible) {
+    return null;
+  }
 
   return (
     <Standard preset="home">
