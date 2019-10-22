03fcd5893def2b61abdb8d1c20017ab1c7f5090f
Abylay Murzashev <abylay.murzashev@gmail.com>
2019-10-22 17:51:06 +0600

    first screen done

--- src/components/pages/Home/components/Information/components/Inputs/components/Submit/index.js
+++ src/components/pages/Home/components/Information/components/Inputs/components/Submit/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import Input from 'components/molecules/Input';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import toggleTopline from 'duck/actions/topline';
 import InputWrap from '../InputWrap';
 import checkInputs from './utils';
 
 const Submit = props => {
   const onClick = () => {
-    const { bindTopline } = props;
+    const { bindTopline, history } = props;
     const areInputsValid = checkInputs(props);
     if (areInputsValid) {
       // proceed to checkout
+      history.push('/checkout');
     } else {
       // show topline error
       bindTopline('Заполните все поля и нужные галочки');
@@ -27,9 +29,12 @@ const Submit = props => {
 
 Submit.propTypes = {
   bindTopline: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
 };
 const mapStateToProps = state => state;
 const mapDispatchToProps = {
   bindTopline: toggleTopline,
 };
-export default connect(mapStateToProps, mapDispatchToProps)(Submit);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Submit));
