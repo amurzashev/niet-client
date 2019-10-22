2a09ac9df9bbec09de9fb9919634cd620fbd0ffe
Abylay Murzashev <abylay.murzashev@gmail.com>
2019-10-22 16:03:58 +0600

    baseURL setup

--- src/duck/actions/index.js
+++ src/duck/actions/index.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 
 let baseURL;
 
-if (process.env.NODE_ENV === 'development') {
-  baseURL = 'http://localhost:3001/api/v1/';
-}
 if (process.env.NODE_ENV === 'production') {
   baseURL = 'https://niet-server.herokuapp.com/api/v1/';
+}
+if (process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:3001/api/v1/';
 }
 
 export const apiRequest = axios.create({
